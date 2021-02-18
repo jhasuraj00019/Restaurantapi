48568b4f2667996f7a15344db8b56a7ce27c8d5b
Suraj Jha <62363414+jhasuraj00019@users.noreply.github.com>
2021-02-18 09:39:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port  = process.env.port || 1200;
+const port  = process.env.PORT || 1200;
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 const bodyParser = require('body-parser');
